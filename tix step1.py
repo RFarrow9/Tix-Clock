ecb63c20fc9cc30ba21857ade8bc24f7ca4ae974
KFitz91 <49559736+KFitz91@users.noreply.github.com>
2019-04-12 13:52:04 +0100

    Add files via upload

---
+++ tix step1.py
@@ -0,0 +1,35 @@
+import time
+import sys
+
+while True:
+    from datetime import datetime
+    now = datetime.now()
+    #print ("%s:%s:%s" % (now.hour,now.minute,now.second),end=''),
+    #print("\r")
+    
+    hour_digit_one = now.hour//10
+    hour_digit_two = now.hour%10
+
+    minute_digit_one = now.minute//10
+    minute_digit_two = now.minute%10
+
+    sec_digit_one = now.second//10
+    sec_digit_two = now.second%10
+
+    str_hour1 = "O"*hour_digit_one
+    str_hour2 = "O"*hour_digit_two
+    str_min1 = "O"*minute_digit_one
+    str_min2 = "O"*minute_digit_two
+    str_sec1 = "O"*sec_digit_one
+    str_sec2 = "O"*sec_digit_two
+
+    print(str_hour1," ",str_hour2," ",str_min1," ",str_min2," ",str_sec1," ",str_sec2)
+    
+    time.sleep(1)
+   
+
+
+
+    
+
+
